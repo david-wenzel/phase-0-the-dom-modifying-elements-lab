7ce6a9175ac2eec45dda944d163facac6d3b3f50
David Wenzel <dwenzel95@gmail.com>
2022-03-22 20:24:12 -0500

    commit tree db19da03688f627590c96e8d9b41e11b5843a3d9

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+let remove = document.getElementById('main');
+remove.remove();
+//removes 'main' by id from html in dom using javascript method
+let newHeader = document.createElement("h1");
+//defines newHeader as dom element in h1
+newHeader.id +="victory";
+//logs the id for newHeader as victory
+newHeader.textContent = 'David is the champion'
+//inserts text content for h1 id #vicotry all within the dom 
